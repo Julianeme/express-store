c90213ea0b07c2aee68c52df06412e433d817149
Julianeme <juliantve@gmail.com>
2023-08-30 10:22:08 -0500

    chore: update node version on json package file

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const routerApiV2 = require('./routes/v2Routes')
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //---UTILIZANDO PRNCIPIO DE UNICA RESPONSABILIDAD---
 
